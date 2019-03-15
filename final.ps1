b85a3ffd0cdb5ecfdc3f42b2d57e1f6a88f80276
David <6669245+dxking@users.noreply.github.com>
2019-03-14 20:41:21 -0700

    Add files via upload

--- final.ps1
+++ final.ps1
@@ -1,22 +1,52 @@
-﻿<#
-Your script should process each of the given .txt files and check to see if the .txt file's MD5 hash matches the content of it's corresponding .md5 file
-So in the case of brown.txt and brown.md5 you should calculate the MD5 hash of brown.txt and see if it matches the content of brown.md5
+﻿# Set-FolderLocation creates a Windows folder browser in order to capture user input for the folder containing .txt and .md5 files
+function Set-FolderLocation{
+    Add-Type -AssemblyName System.Windows.Forms
+    $folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
+    $folderBrowser.Description = 'Select the folder containing .txt/.md5 files'
+    # $result attempts to make the folder browser the top-most element on the screen
+    $result = $folderBrowser.ShowDialog((New-Object System.Windows.Forms.Form -Property @{TopMost = $true }))
 
-If a file's hash does not match, print "Invalid file: $filename" to the console (so in this case it would print "Invalid file: brown.txt), then write that file's name to a new file: invalid.txt
-Comment your script sensibly, highlighting what each logical block of code in your script is doing
-#>
-$txtFiles = Get-ChildItem "C:\Users\david.king\Documents\IT 120\Final\*.txt"
-
-foreach($file in $txtFiles){
-    $fileHash = Get-FileHash $file -Algorithm MD5
-    
-    $noExtension,$extension = $file.Name -Split(".txt")  
-    $checkSum = Get-Content "$noExtension.md5"
-
-    if($checkSum -ne $fileHash.Hash){
-        Write-Host "Invalid file: $file.Name"
-        Add-Content -Path invalid.txt -Value $file.Name 
+    if($result -eq [Windows.Forms.DialogResult]::OK){
+        $folderBrowser.SelectedPath
     }
-    #$file.Name + " " + $fileHash.Hash
-    #"$noExtension.md5" + " " + $checkSum
+    else {
+        exit
+    }
+}
+
+# Refine folder location into variables
+$folder = Set-FolderLocation
+$invalidPath = $folder + '\invalid.txt'
+$files = $folder | Get-ChildItem
+$fileNames = $files.Name
+$txtFiles = @()
+
+# Add only .txt files to the $txtFiles array
+foreach($f in $fileNames){
+    $filePath = $folder + '\' + $f
+    $extension = [IO.Path]::GetExtension($f)
+    if($extension -eq '.txt'){
+        $txtFiles += $filePath 
+    }
+}
+
+# If invalid.txt exists then clear contents
+if(Test-Path -Path $invalidPath -PathType Leaf){Clear-Content -Path $invalidPath}
+
+# Loop through each .txt file and get the MD5 sum
+foreach($f in $txtFiles){
+    # If invalid.txt is $f skip this loop
+    if($f -eq $invalidPath){continue}
+
+    $fileHash = Get-FileHash $f -Algorithm MD5
+    $fileHash = $fileHash.Hash
+    # Switch file from $f.txt to $f.md5
+    $sumFile = $f.Replace('.txt', '.md5')
+    $checkSum = Get-Content $sumFile
+
+    # Compare checksums
+    if($fileHash -ne $checkSum){
+        Write-Host "Invalid file: $f"
+        Add-Content -Path $invalidPath -Value $f
+    }
 }
